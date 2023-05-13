6bab424073e63953d163870eff50fbf29b505862
severoleonardo <2020.leonardosevero@gmail.com>
2023-05-13 11:32:35 -0300

    added details

--- W04 - Solving Problems Using Variables and Expressions/speed_of_a_falling_object.py
+++ W04 - Solving Problems Using Variables and Expressions/speed_of_a_falling_object.py
@@ -2,12 +2,13 @@
 Title: 04 Team Activity - Speed of a Falling Object
 Author: Leonardo Severo
 
-Purpose: Determine how fast an object will fall. 
+Purpose: Determine how fast an object will fall using the formula: 
+v(t) = sqrt(mg/c) * (1 - exp((-sqrt(mgc)/m)*t))
 """
 
 import math
 
-print("Welcome to the velocity calculator. Please enter the following: ")
+print("Welcome to the velocity calculator. Please enter the following:\n")
 
 m = float(input("\nMass (in Kg): "))
 g = float(input("Gravity (in m/s^2, 9.8 for Earth, 24 for Jupter): "))
@@ -17,13 +18,15 @@ A = float(input("Cross sectional area (in m^2): "))
 C = float(input("Drag constant (0.5 for sphere, 1.1 for cylinder): "))
 
 
-#Calculate 
+# First calculate c = 1/2 p A C
 c = (1 / 2) * p * A * C   
+
+# Now calculate the velocity v(t) = sqrt(mg/c) * (1 - exp((-sqrt(mgc)/m)*t))
 velocity = math.sqrt(m * g / c) * (1 - math.exp((- math.sqrt(m * g * c)/ m ) * t ))
         
 
-#Resolution 
-
+#Display
+print() # display a blank line
 print(f"\nThe inner value of c is: {c:.3f}")
 print(f"The velocity after {t:.1f} seconds is: {velocity:.3f} m/s")
 
